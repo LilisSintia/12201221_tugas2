af2751ae518f2651888b41e01fc2b89fcec168c5
LilisSintia <lilissintialubis25@gmail.com>
2021-09-27 09:59:56 +0700

    menambahkan tugas2 dan biodata diri

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama Saya Lilis Sintia, Saya tinggal di Langkaplancar, Pangandaran. Olahraga yang saya sukai adalah Volly Ball. No. Hp yang bisa di hubungi 082217440201.";
+	}
+}
